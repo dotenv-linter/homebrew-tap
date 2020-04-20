741fb08e5c8dec0fbbe1e66c861bd12499465fc6
Grachev Mikhail <work@mgrachev.com>
2020-04-20 12:22:28 +0300

    Update dotenv-linter.rb

--- Formula/dotenv-linter.rb
+++ Formula/dotenv-linter.rb
@@ -1,15 +1,15 @@
 class DotenvLinter < Formula
   desc "Linter for .env files. Written in Rust."
   homepage "https://github.com/mgrachev/dotenv-linter"
-  version "1.1.2"
+  version "1.2.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/mgrachev/dotenv-linter/releases/download/v1.1.2/dotenv-linter-darwin-x86_64.tar.gz"
-    sha256 "708e6b48209b8746ce0eed98291602ab3978d22b54a53a1f5395cb07b1925ab2"
+    url "https://github.com/mgrachev/dotenv-linter/releases/download/v1.2.0/dotenv-linter-darwin-x86_64.tar.gz"
+    sha256 "77bb9375af19a8b0acd1423bc920801add772c874661ebd41530f50d45f61fae"
   elsif OS.linux?
-    url "https://github.com/mgrachev/dotenv-linter/releases/download/v1.1.2/dotenv-linter-linux-x86_64.tar.gz"
-    sha256 "df388e53442e617120ce5a57ea6868ae055e9c232563d2658220261edbc0c690"
+    url "https://github.com/mgrachev/dotenv-linter/releases/download/v1.2.0/dotenv-linter-linux-x86_64.tar.gz"
+    sha256 "eb840363c5f21c6cefc53edc6516e6978f0100e00f2c58b6452a23caa3652312"
   end
 
   def install
