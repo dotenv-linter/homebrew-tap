2d9aa00f446da770edc054eb178b4459e1de19c8
Jaslo <jaslo@jaslogic.tech>
2020-10-01 19:21:21 +0200

    Add ability to install latest version (#3)
    This commits adds support for the --HEAD flag in Homebrew as
    described in this issue:
    https://github.com/dotenv-linter/dotenv-linter/issues/295

--- Formula/dotenv-linter.rb
+++ Formula/dotenv-linter.rb
@@ -2,18 +2,30 @@ class DotenvLinter < Formula
   desc "Lightning-fast linter for .env files. Written in Rust."
   homepage "https://github.com/dotenv-linter/dotenv-linter"
   version "2.1.0"
-  bottle :unneeded
 
-  if OS.mac?
-    url "https://github.com/dotenv-linter/dotenv-linter/releases/download/v2.1.0/dotenv-linter-darwin-x86_64.tar.gz"
-    sha256 "750606d7404a2d8224c7a8ad047653eabe4995d1cd3edd89310179eebbbedb60"
-  elsif OS.linux?
-    url "https://github.com/dotenv-linter/dotenv-linter/releases/download/v2.1.0/dotenv-linter-linux-x86_64.tar.gz"
-    sha256 "26783c3f2e30452e2669c8677561ec46880e2f66eaf75c129db69e4b81e2f9da"
+  stable do
+    if OS.mac?
+      url "https://github.com/dotenv-linter/dotenv-linter/releases/download/v2.1.0/dotenv-linter-darwin-x86_64.tar.gz"
+      sha256 "750606d7404a2d8224c7a8ad047653eabe4995d1cd3edd89310179eebbbedb60"
+    elsif OS.linux?
+      url "https://github.com/dotenv-linter/dotenv-linter/releases/download/v2.1.0/dotenv-linter-linux-x86_64.tar.gz"
+      sha256 "26783c3f2e30452e2669c8677561ec46880e2f66eaf75c129db69e4b81e2f9da"
+    end
   end
 
+  head do
+    url "https://github.com/dotenv-linter/dotenv-linter.git"
+    depends_on "rust" => :build
+  end
+
+  bottle :unneeded
+
   def install
-    bin.install "dotenv-linter"
+    if build.head?
+      system "cargo", "install", *std_cargo_args
+    else
+      bin.install "dotenv-linter"
+    end
   end
 
   test do
